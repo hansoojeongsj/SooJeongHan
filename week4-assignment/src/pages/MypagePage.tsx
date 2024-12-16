509cbcd4911c9d82ebc58133dab74d7e6c412a64
hansoojeongsj <tnwjd3758@duksung.ac.kr>
2024-12-17 03:01:53 +0900

    feat: 헤더 string타입 -> 올 수 있는 값만 명시

--- week4-assignment/src/pages/MypagePage.tsx
+++ week4-assignment/src/pages/MypagePage.tsx
@@ -4,21 +4,20 @@ import MypageHobby from '../components/Mypage/MypageHobby';
 import MypageInfo from '../components/Mypage/MypageInfo';
 
 const MypagePage = () => {
-  const [selectedComponent, setSelectedComponent] = useState<string>('취미');
+  const [selectedComponent, setSelectedComponent] = useState<'취미' | '내 정보'>('취미');
 
   const handleComponentSelect = (component: string) => {
-    setSelectedComponent(component);
+    setSelectedComponent(component as '취미' | '내 정보');
   };
 
   return (
     <div>
       <MypageHeader onComponentSelect={handleComponentSelect} />
       <div>
-        {selectedComponent === '취미' && <MypageHobby/>}
-        {selectedComponent === '내 정보' && <MypageInfo/>}
+        {selectedComponent === '취미' && <MypageHobby />}
+        {selectedComponent === '내 정보' && <MypageInfo />}
       </div>
     </div>
   );
 };
-
 export default MypagePage;
